93a6977ac1f577b33cad43d4ffff7aa894738153
Franziska Neumeister <info@franziska-neumeister.de>
2015-03-06 23:28:45 +0100

    Add swipe handler

--- app/src/main/java/de/franziskaneumeister/counterswipe/gestures/SwipeOverCounterHandler.java
+++ app/src/main/java/de/franziskaneumeister/counterswipe/gestures/SwipeOverCounterHandler.java
@@ -3,16 +3,23 @@ package de.franziskaneumeister.counterswipe.gestures;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 
 public class SwipeOverCounterHandler implements GestureDetector.OnGestureListener{
-    
+
+    private static final double TRANSLATION_LIMIT = 100;
+    private View mView;
+
     @Override
     public boolean onDown(MotionEvent e) {
+        Log.d("touch", "Tap went down");
         return false;
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        double xOffset = e1.getX() - e2.getX();
+        xOffset = Math.min(xOffset, TRANSLATION_LIMIT);
         return false;
     }
 
@@ -37,4 +44,7 @@ public class SwipeOverCounterHandler implements GestureDetector.OnGestureListene
 
     }
 
+    public void setView(View view) {
+        mView = view;
+    }
 }
